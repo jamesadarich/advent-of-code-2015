5ac613c2f11a21b2569d2bbfcd91a3059d88c9c9
James Richford <jamesrichford@googlemail.com>
2015-12-23 19:58:51 +0000

    Add remaining days

---
+++ challenges/day-23/part-1/inputs.ts
@@ -0,0 +1,15 @@
+export class Inputs {
+  public static examples: any[] = [
+    {
+      input: 'abcdefgh',
+      expected: 'abcdffaa'
+   },
+
+     {
+       input: 'ghijklmn',
+       expected: 'ghjaabcc'
+     }
+  ];
+
+  public static challenge: any = 'hepxcrrq';
+}
