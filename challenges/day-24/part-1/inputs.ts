426447dc83dd1b58b7081ab6feb2c8d7d59b91de
James Richford <jamesrichford@googlemail.com>
2015-12-24 08:18:14 +0000

    Start Day 24

--- challenges/day-24/part-1/inputs.ts
+++ challenges/day-24/part-1/inputs.ts
@@ -1,15 +1,47 @@
 export class Inputs {
   public static examples: any[] = [
     {
-      input: 'abcdefgh',
-      expected: 'abcdffaa'
-   },
-
-     {
-       input: 'ghijklmn',
-       expected: 'ghjaabcc'
-     }
+      input: `1
+      2
+      3
+      4
+      5
+      7
+      8
+      9
+      10
+      11`,
+      expected: 99
+   }
   ];
 
-  public static challenge: any = 'hepxcrrq';
+  public static challenge: any = `1
+2
+3
+7
+11
+13
+17
+19
+23
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+101
+103
+107
+109
+113`;
 }
--- challenges/day-24/part-1/solution.ts
+++ challenges/day-24/part-1/solution.ts
@@ -1,5 +1,30 @@
 export class Solution {
 
   public solve(input) {
+    let presents = input.split('\n').map((present) => parseInt(present.trim()));
+
+
+    let balancedSection = presents.reduce((x, y) => x + y);
+
+
+    let validConfigurations = this._findCombos(presents, balancedSection, [], [[], [], []])
+
+    return validConfigurations;
+   }
+
+   private _findCombos(remainingPossibilities: Array<number>, target: number, combos: Array<Array<Array<number>>>, currentCombo: Array<Array<number>>): Array<Array<Array<number>>> {
+
+      for (let i = 0; i < remainingPossibilities.length; i++) {
+        for (let j = 0; j < currentCombo.length; j++) {
+          if (currentCombo[j].length > 0 && currentCombo[j].reduce((x, y) => x + y) + remainingPossibilities[i] > target){
+            continue;
+          }
+          this._findCombos(remainingPossibilities.slice(i + 1, remainingPossibilities.length), target, combos, currentCombo.concat([remainingPossibilities[i]]));
+        }
+     }
+
+     combos.push(currentCombo);
+
+      return combos;
    }
 }
